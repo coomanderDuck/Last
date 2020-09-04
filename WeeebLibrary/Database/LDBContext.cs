764ce453eaffcd029d847df6a911232d13526526
coomanderDuck <egorcos53@gmail.com>
2020-09-05 02:27:18 +0300

    first commit

---
+++ WeeebLibrary/Database/LDBContext.cs
@@ -0,0 +1,25 @@
+﻿using WeeebLibrary.Database.Entitys;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WeeebLibrary.Database
+{
+    public class LDBContext
+    {
+        public class LDbContext : IdentityDbContext<User>
+        {
+            public DbSet<Microsoft.AspNetCore.Identity.IdentityUserClaim<Guid>> IdentityUserClaims { get; set; }
+
+            public LDbContext(DbContextOptions<LDbContext> options)
+                : base(options)
+            {
+                Database.EnsureCreated();
+            }
+        }
+    }
+}
---
+++ WeeebLibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WeeebLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
